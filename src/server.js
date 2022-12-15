fa8530c2306dcd8800a3b02059d5c04c590fa521
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-12-15 16:30:41 -0300

    feat: creating server.js

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(4000, () => console.log(`Server running in PORT: 4000`));
