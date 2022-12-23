3a9a76e10a8e8a72255a397c8d3b77a1e4ad1c6c
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-12-23 16:40:02 -0300

    .env example

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js"
 import urlRoute from "./routes/urlRoute.js"
 
 const app = express();
 
+dotenv.config();
 app.use(express.json());
 app.use(cors());
 
 app.use(userRoute);
 app.use(urlRoute);
 
-app.listen(4000, () => console.log(`Server running in PORT: 4000`));
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Server running in PORT: ${PORT}`));
